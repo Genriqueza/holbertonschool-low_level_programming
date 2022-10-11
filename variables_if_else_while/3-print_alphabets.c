84296fcc37e5bf1c2efe4efdd5e825de517f8c7b
Gehenriqueza <entretenimiento704@gmail.com>
2022-10-11 13:25:33 -0700

    program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,9 @@ char letter;
 char Letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar (letter);
-
 for (Letter = 'A'; Letter <= 'Z'; Letter++)
 putchar (Letter);
 putchar ('\n');
-	
+
 return (0);
 }
