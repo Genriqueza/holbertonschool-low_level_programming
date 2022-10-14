bf1e6f41ed70b2bbb18b60323f95821cc188649d
Gehenriqueza <entretenimiento704@gmail.com>
2022-10-14 08:47:28 -0700

    header file of project.

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+* Header file containing prototypes for all functions
+* in the 0x02. C - Functions, nested loops| project.
+*/
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif /* _MAIN_H_ */
+
